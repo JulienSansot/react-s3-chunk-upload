19c7d848b6177e61cf0efe91529446efdba6b051
Julien <julien.sansot@orange.com>
2015-07-02 16:03:15 +0900

    browsing files

---
+++ app/assets/javascripts/components/repository-table-row-folder.js.jsx
@@ -0,0 +1,19 @@
+RepositoryTableRowFolder = React.createClass({
+
+  render: function() {
+
+  	// var styleRow = { cursor: 'pointer' };
+  	// var styleText = { textDecoration: 'underline'};
+
+  	return (
+  		<tr>
+  			<td onClick={this.props.onGoIntoFolder}>
+  				<i className='fa fa-folder'></i>&nbsp;
+  				<a href="#">{this.props.folder.name}</a>
+  			</td>
+        <td></td>
+  		</tr>
+  	);
+  }
+
+})
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'home#index'
+
+  get '/get_files_as_tree' => 'home#get_files_as_tree'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
